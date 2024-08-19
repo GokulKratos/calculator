f9b46af5ab59ac3fcba6ab652abc3d5cb88b2d83
gokulkumar <gokulkumarofficial@outlook.com>
2024-08-19 18:45:35 +0530

    update to restController

--- src/main/java/com/calc/controller/CalculatorController.java
+++ src/main/java/com/calc/controller/CalculatorController.java
@@ -3,13 +3,11 @@ package com.calc.controller;
 import com.calc.model.Calculator;
 import com.calc.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/Calculator")
+@RestController
 public class CalculatorController {
 
     @Autowired
